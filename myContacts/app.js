48a1bdef75bbec5c85bc5ef7cdc90985130cd287
22nortpl <minsanglee03@gmail.com>
2026-07-10 21:39:47 +0900

    06-1

--- myContacts/app.js
+++ myContacts/app.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
+const port = 3000;
 
-app.listen(3000, () => {
-  console.log('서버 실행 중');
-})
+app.get("/", (req, res) => {
+  res.status(200);
+  res.send("Hello, Node!");
+});
+
+app.get("/contacts", (req, res) => {
+  res.status(200);
+  res.send("Contacts Page");
+});
+
+app.listen(port, () => {
+  console.log(`${port}번 포트에서 서버 실행 중!`);
+});
